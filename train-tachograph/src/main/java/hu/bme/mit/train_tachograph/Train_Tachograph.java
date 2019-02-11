657875cc86fbd82a8970ea0285727c43dd3f5a76
fdora73 <dora.foldi73@gmail.com>
2019-02-11 08:45:38 -0800

    tacho

--- train-tachograph/src/main/java/hu/bme/mit/train_tachograph/Train_Tachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train_tachograph/Train_Tachograph.java
@@ -5,4 +5,14 @@ import com.google.common.collect.Table;
 
 public class Train_Tachograph {
     private Table<Integer,Integer,Integer> table = HashBasedTable.create();
+
+    public void addElement()
+    {
+        table.put(0,0,0);
+    }
+     public Boolean HasElements(){
+        return !table.isEmpty();
+     }
 }
+
+
