b3aeafb42c628be4021ae6fa908d315e064909ce
fdora73 <dora.foldi73@gmail.com>
2019-02-11 08:16:17 -0800

    tachograph

---
+++ train-tachograph/src/main/java/hu/bme/mit/train_tachograph/Train_Tachograph.java
@@ -0,0 +1,8 @@
+package hu.bme.mit.train_tachograph;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+public class Train_Tachograph {
+    private Table<Integer,Integer,Integer> table = HashBasedTable.create();
+}
